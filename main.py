d1daa7f31edbed414a8aab447c1ccdf159d787a0
TH <00001116754629sp@al.educacao.sp.gov.br>
2024-06-28 10:58:02 -0300

    aluno 2024

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
-[09:29] Joao Vitor Carlos De Almeida Araujo
-claudiaNotas = [10, 7, 5, 6]
-joaoNotas = [3, 5, 6, 5]
-thiagoNotas = [10, 7, 7, 9]
-samuelNotas = [10, 9, 9, 8]
- 
-# Faz a média das notas do bimestre
-claudiaMedia = sum(claudiaNotas ) / 4
-joaoMedia = sum(joaoNotas) / 4
-thiagoMedia = sum(thiagoNotas) / 4
-samuelMedia = sum(samuelNotas ) / 4
- 
-# Determina se o aluno passou ou não de acordo com o valor da sua média
-if claudiaMedia >= 5:
-    claudiaAprovacao = 'Aprovado'
-else:
-    claudiaAprovacao = 'Reprovado'
- 
-if joaoMedia >= 5:
-    joaoAprovacao = 'Aprovado'
-else:
-    joaoAprovacao = 'Reprovado'
+try:
+    nomeAluno = (input('Digite o nome do aluno: ')).title()
+    primeiraNota = int(input('Primeira Nota do aluno: '))
+    segundaNota = int(input('Segunda Nota do aluno: '))
+    terceiraNota = int(input('terceira Nota do aluno: '))
+    quartaNota = int(input('quarta Nota do aluno: '))
+    mediaTotal = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4
+
+    if mediaTotal >= 5:
+        alunoAprovacao = 'Aprovado'
+    else:
+        alunoAprovacao = 'Reprovado'
+    # Mostra a nota do aluno
+    print(f'\tNota média do aluno {nomeAluno}\t')
+    print(f'\nMédia: {mediaTotal}\n1° Bimestre: {primeiraNota}\n2° Bimestre: {segundaNota}\n3° Bimestre: {terceiraNota}\n4° Bimestre: {quartaNota}')
+    print(f'\nSITUAÇÃO: {alunoAprovacao}')
    
-if thiagoMedia >= 5:
-    thiagoAprovacao = 'Aprovado'
-else:
-    thiagoAprovacao = 'Reprovado'
-   
-if samuelMedia >= 5:
-    samuelAprovacao = 'Aprovado'
-else:
-    samuelAprovacao = 'Reprovado'
-   
-# Mostra no terminal todos os valores pedidos
-print('===\tNota média dos alunos\t===')
-print(f'Claudia: {claudiaMedia}\t{claudiaAprovacao}\nJoão: {joaoMedia}\t{joaoAprovacao}\nThiago: {thiagoMedia}\t{thiagoAprovacao}\nSamuel: {samuelMedia}\t{samuelAprovacao} ')
+except:
+    print('Ocorreu um erro...')
  
